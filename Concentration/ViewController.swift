15e5725c356eeee5343e7594dec22c7a628eefec
Adith Rai <rai_adith@yahoo.co.in>
2019-02-03 16:29:39 -0600

    First Commit

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Adith Rai on 2/3/19.
+//  Copyright © 2019 Adith Rai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCard(withEmoji: "👻", on: sender)
+    }
+    func flipCard(withEmoji emoji: String, on button: UIButton){
+        
+    }
+    
+}
+
