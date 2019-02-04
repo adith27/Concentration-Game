24d8f1802bd57db4bd822d319ab7c6419a671126
Mahnoor Wazirzada <mwazirzada@Mahnoors-MacBook.local>
2019-02-03 21:45:33 -0600

    Initial Card Flip Created

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -13,8 +13,17 @@ class ViewController: UIViewController {
     @IBAction func touchCard(_ sender: UIButton) {
         flipCard(withEmoji: "👻", on: sender)
     }
+    
     func flipCard(withEmoji emoji: String, on button: UIButton){
-        
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1)
+            
+        }
+        else {
+            button.setTitle(emoji, for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
     }
     
 }
